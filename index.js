e36df92933bd90b465b0a6f66ebd51b3c5020d94
Léo Serrão Barbosa <leo_serrao@hotmail.com>
2022-04-09 16:04:57 -0300

    Implementando algumas mensagens

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const TOKEN = '';
+const TOKEN = 'OTYyMzk4NjAxMTk0MTMxNTU4.YlG9iw.9fa6b9cXdS9SM3AbqpwgB3p7yB0';
 
 client.on("ready", () => {
     console.log('Bot online!');
 });
 
 client.on("message", (msg) => {
-
+    if (msg.content === 'Olá') {
+        msg.reply('Olá');
+    }
+    if (msg.content === 'oi') {
+        msg.channel.send('Olá ' + msg.author.username);
+    }
 });
 
 client.login(TOKEN);
